72f881075cf709013b85aae24cfadf00df4dbe4b
Firmin Sosso Ngoutte <firmin.sossongoutte@soprasteria.com>
2020-01-17 10:33:57 +0100

    Initial Commit

---
+++ src/test/java/fn3s/java/spring/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fn3s.java.spring.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
